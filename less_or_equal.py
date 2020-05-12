c59e135554f722f95bf60aa35c1e84c2540bbdb9
abdelrhmanshokr <abdelrhmanshokr13@gmail.com>
2020-05-12 16:18:42 +0200

    solved the less or equal problem, the site has an issue with my answer but I think it's the correct one

--- less_or_equal.py
+++ less_or_equal.py
@@ -16,7 +16,7 @@ def find_max(a):
 def main():
     #getting the size n and the number k 
     n, k = map(int, input().split())
-    
+
     #getting the array a 
     a = list(map(int, input().strip().split()))[:n] 
 
@@ -27,25 +27,30 @@ def main():
     k_new = k  
 
     #loop over all x values to find one that fits 
-    for x in range(find_max(a)): 
-        print('this is x from up top =', x)
-        if k_new == 0: 
-            break
-        else:
-            k_new = k 
+    for x in range(10):
+        # print('this is x from up top =', x)
+        k_new = 0 
+        started_counting = False
     
-            for i in range(n):
-                #if k = 0 then this x fits our requirements and we're done
-                if k_new == 0:
-                    print('this is x from down =', x)
-                    return x 
-                elif a[i] <= x:
-                    print('this is the old k =', k_new)
-                    print('this is i =', i)
-                    print('this is a[i] =', a[i])
-                    k_new -= 1 
-                    print('this is the new k =', k_new)
-        print('================================')
+        for i in range(n):
+            # print('this is intial i', i)
+            # if k = 0 then this x fits our requirements and we're done 
+            if a[i] <= x:
+                # print('this is the old k =', k_new)
+                # print('this is i =', i)
+                # print('this is a[i] =', a[i])
+                k_new += 1 
+                started_counting = True
+                #print('this is the new k =', k_new)
+                
+        #print('======NEW X CYCLE======')
+
+        if k_new == k and started_counting == True:
+            return x 
+    
+    else:
+        return -1
+      
 
 
 
